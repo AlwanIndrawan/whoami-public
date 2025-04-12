610358f1c3ef1d25ab2b2a32ab5fb956cacf391b
AlwanIndrawan <alwaanindrawan@gmail.com>
2025-04-12 20:38:35 +0800

    tugas1

---
+++ belajar1.js
@@ -0,0 +1,10 @@
+const biodata = {
+    nama : "Allone",
+    alamat : "Dungeon, no.09",
+    instagram : "@wawan",
+};
+
+console.log("PROFIL");
+console.log(`Nama: ${biodata.nama}`);
+console.log(`Alamat: ${biodata.alamat}`);
+console.log(`Instagram: ${biodata.instagram}`);
